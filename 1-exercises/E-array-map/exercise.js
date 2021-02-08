5a7df9bb57c4a6f5de5979889f33b1b9e82ad1db
Kara Howard <kara.mh191@gmail.com>
2021-02-08 10:14:02 +0000

    E - array map complete
    Completed E - array map exercise

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -2,3 +2,20 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+
+function multiplyBy100 (number) {
+    return number * 100;
+}
+
+// let multipliedNumbers = numbers.map(multiplyBy100);
+
+// let multipliedNumbers = numbers.map(function multiplyBy100 (number) {
+//     return number * 100;
+// });
+
+// let multipliedNumbers = numbers.map(number => {
+//     return number * 100;
+// });
+
+let multipliedNumbers = numbers.map(number => number * 100);
+console.log(multipliedNumbers);
